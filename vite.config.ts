ed5d6420ec19e6b19dca5036dd460f181c469b64
Isis1010 <65567258+Isis1010@users.noreply.github.com>
2026-01-12 08:15:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,89 +8,92 @@ import { logChunkSizePlugin } from './lib/vite-log-chunk-size.ts'
 import { themeColorsPlugin } from './lib/vite-plugin-theme-colors.ts'
 
 const getAutoImportPlugin = (dts: string | false = false) =>
-	AutoImport({
-		dts,
-		imports: [
-			{
-				'$paraglide/messages': [['*', 'm']],
-				'$lib/stores/player/use-store.ts': ['usePlayer'],
-				'$lib/stores/main/use-store.ts': ['useMainStore'],
-				'$lib/components/menu/MenuRenderer.svelte': ['useMenu'],
-				'tiny-invariant': [['default', 'invariant']],
-				svelte: ['untrack'],
-			},
-		],
-	})
+        AutoImport({
+                dts,
+                imports: [
+                        {
+                                '$paraglide/messages': [['*', 'm']],
+                                '$lib/stores/player/use-store.ts': ['usePlayer'],
+                                '$lib/stores/main/use-store.ts': ['useMainStore'],
+                                '$lib/components/menu/MenuRenderer.svelte': ['useMenu'],
+                                'tiny-invariant': [['default', 'invariant']],
+                                svelte: ['untrack'],
+                        },
+                ],
+        })
 
 export default defineConfig({
-	server: {
-		fs: {
-			allow: ['./.generated'],
-		},
-		warmup: {
-			// Avoids page reloading in Dev mode. When vite supports bundled-dev mode this can be removed.
-			clientFiles: [
-				'src/lib/components/**/*.svelte',
-				'src/lib/library/scan-actions/scanner/worker.ts',
-			],
-		},
-	},
-	// Tell Vitest to use the `browser` entry points in `package.json` files, even though it's running in Node
-	resolve: process.env.VITEST ? { conditions: ['browser'] } : undefined,
-	build: {
-		target: ['chrome130', 'safari18'],
-		rolldownOptions: {
-			output: {
-				legalComments: 'none',
-				advancedChunks: {
-					groups: [
-						{
-							// Merge all css into a single file
-							name: 'styles',
-							test: /\.css$/,
-							minModuleSize: 0,
-							priority: 100,
-						},
-						{
-							// Merge smaller chunks than together
-							name: 'small-chunks',
-							maxModuleSize: 1 * 1024,
-						},
-					],
-				},
-			},
-		},
-	},
-	worker: {
-		format: 'es',
-		plugins: () => [getAutoImportPlugin()],
-	},
-	plugins: [
-		themeColorsPlugin({
-			defaultColorSeed: '#cc9724',
-			output: `${import.meta.dirname}/.generated/theme-colors.css`,
-		}),
-		imagetools(),
-		tailwindcss(),
-		sveltekit(),
-		paraglideVitePlugin({
-			project: './project.inlang',
-			outdir: './.generated/paraglide',
-			strategy: ['baseLocale'],
-			isServer: 'import.meta.env.SSR',
-		}),
-		getAutoImportPlugin('./.generated/types/auto-imports.d.ts'),
-		logChunkSizePlugin(),
-		{
-			name: 'ssr-config',
-			config(config) {
-				const isSsr = config?.build?.ssr
+        server: {
+                host: '0.0.0.0',
+                port: 5000,
+                allowedHosts: true,
+                fs: {
+                        allow: ['./.generated'],
+                },
+                warmup: {
+                        // Avoids page reloading in Dev mode. When vite supports bundled-dev mode this can be removed.
+                        clientFiles: [
+                                'src/lib/components/**/*.svelte',
+                                'src/lib/library/scan-actions/scanner/worker.ts',
+                        ],
+                },
+        },
+        // Tell Vitest to use the `browser` entry points in `package.json` files, even though it's running in Node
+        resolve: process.env.VITEST ? { conditions: ['browser'] } : undefined,
+        build: {
+                target: ['chrome130', 'safari18'],
+                rolldownOptions: {
+                        output: {
+                                legalComments: 'none',
+                                advancedChunks: {
+                                        groups: [
+                                                {
+                                                        // Merge all css into a single file
+                                                        name: 'styles',
+                                                        test: /\.css$/,
+                                                        minModuleSize: 0,
+                                                        priority: 100,
+                                                },
+                                                {
+                                                        // Merge smaller chunks than together
+                                                        name: 'small-chunks',
+                                                        maxModuleSize: 1 * 1024,
+                                                },
+                                        ],
+                                },
+                        },
+                },
+        },
+        worker: {
+                format: 'es',
+                plugins: () => [getAutoImportPlugin()],
+        },
+        plugins: [
+                themeColorsPlugin({
+                        defaultColorSeed: '#cc9724',
+                        output: `${import.meta.dirname}/.generated/theme-colors.css`,
+                }),
+                imagetools(),
+                tailwindcss(),
+                sveltekit(),
+                paraglideVitePlugin({
+                        project: './project.inlang',
+                        outdir: './.generated/paraglide',
+                        strategy: ['baseLocale'],
+                        isServer: 'import.meta.env.SSR',
+                }),
+                getAutoImportPlugin('./.generated/types/auto-imports.d.ts'),
+                logChunkSizePlugin(),
+                {
+                        name: 'ssr-config',
+                        config(config) {
+                                const isSsr = config?.build?.ssr
 
-				// Since this is mostly SPA, server logs are mostly noise.
-				config.logLevel = isSsr ? 'warn' : 'info'
+                                // Since this is mostly SPA, server logs are mostly noise.
+                                config.logLevel = isSsr ? 'warn' : 'info'
 
-				return config
-			},
-		},
-	],
+                                return config
+                        },
+                },
+        ],
 })
